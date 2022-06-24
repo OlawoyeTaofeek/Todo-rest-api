b281f10abf8939b65c6e85a4b300a85b97c155c0
Olawoye Taofeek <habephe21@gmail.com>
2022-06-25 00:55:12 +0100

    Third Commit

--- TodoApi/urls.py
+++ TodoApi/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('todos/completed', views.TodoCompletedList.as_view()),
-
+    path('todos/list', views.TodoCurrentList.as_view()),
+    path('todos/list/<int:pk>', views.TodoRetrieveDestroy.as_view()),
 ]
--- TodoApi/views.py
+++ TodoApi/views.py
@@ -13,3 +13,27 @@ class TodoCompletedList(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
+
+class TodoCurrentList(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=True).order_by('-datecompleted')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TodoRetrieveDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+    serializer = TodoSerializer(queryset, many=True)
+    permission_class = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        todo = Todo.objects.filter(pk = self.kwargs['pk'], user=self.request.user, datecompleted__isnull=True)
+        if todo.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('This isn\'t your todo so you cant delete it')
